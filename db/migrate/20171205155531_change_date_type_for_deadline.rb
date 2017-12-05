6d12b8259c0d75e5256d4679c19838f32414c5cf
Calvin McGee III <clvnmcgee@outlook.com>
2017-12-05 11:15:22 -0500

    first commit

---
+++ db/migrate/20171205155531_change_date_type_for_deadline.rb
@@ -0,0 +1,12 @@
+class ChangeDateTypeForDeadline < ActiveRecord::Migration[5.1]
+  def self.up
+    change_table :tasks do |t|
+      t.change :deadline, :string
+    end
+  end
+  def self.down
+    change_table :tasks do |t|
+      t.change :deadline, :datetime
+    end
+  end
+end
